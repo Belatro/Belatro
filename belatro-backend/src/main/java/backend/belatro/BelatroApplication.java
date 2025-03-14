c8152ba6e2f6fae179b4a7201a3b30ac2df26f72
lmiholic <lukas.miholiclol@gmail.com>
2025-03-14 17:18:25 +0100

    backend init commit

---
+++ belatro-backend/src/main/java/backend/belatro/BelatroApplication.java
@@ -0,0 +1,13 @@
+package backend.belatro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelatroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelatroApplication.class, args);
+	}
+
+}
---
+++ belatro-backend/src/test/java/backend/belatro/BelatroApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.belatro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelatroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
