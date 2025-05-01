c9162d91499738fe628a1fbc5590d601a4e3d8b8
MarkovicMaks <markovic.maks@gmail.com>
2025-05-01 11:41:39 +0200

    Aded the groundwork for jwt

---
+++ belatro-backend/src/main/java/backend/belatro/security/JwtTokenProvider.java
@@ -0,0 +1,44 @@
+package backend.belatro.security;
+
+import io.jsonwebtoken.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    public String createToken(String subject, long expirationMinutes) {
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + expirationMinutes * 60 * 1000);
+
+        return Jwts.builder()
+                .setSubject(subject)
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(SignatureAlgorithm.HS256, jwtSecret.getBytes())
+                .compact();
+    }
+
+    public String getUsernameFromJWT(String token) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(jwtSecret.getBytes())
+                .parseClaimsJws(token)
+                .getBody();
+        return claims.getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(jwtSecret.getBytes()).parseClaimsJws(token);
+            return true;
+        } catch (JwtException ex) {
+            // Token nije valjan
+        }
+        return false;
+    }
+}
